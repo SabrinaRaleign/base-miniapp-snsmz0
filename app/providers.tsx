61cd3d67d35406f790db468d37ba93424463f4d9
Sabrina Raleign <pukdu4660788@outlook.com>
2026-03-25 15:32:51 +0900

    update app/providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ export function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config} reconnectOnMount>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
